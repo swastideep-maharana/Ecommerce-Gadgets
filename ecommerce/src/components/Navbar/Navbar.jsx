e76bdc6c1b21772f9c0e68d21787c4de4d05e747
swastideep maharana <swastideep67maharana@gmail.com>
2024-06-19 22:32:10 +0530

    added quick link

--- ecommerce/src/components/Navbar/Navbar.jsx
+++ ecommerce/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IoMdSearch } from "react-icons/io";
-import {FaCartShopping} from "react-icons/fa6";
+import {FaCaretDown, FaCartShopping} from "react-icons/fa6";
 import Darkmode from "./Darkmode";
 
 const MenuLinks = [
@@ -9,6 +9,24 @@ const MenuLinks = [
   { id: 3, name: "About", link: "/#about" },
   { id: 4, name: "Blogs", link: "/#blogs" },
 ];
+
+const DropdownLinks = [
+    {
+        id: 1,
+        name: "Trending Products",
+        link: "#/"
+    },
+    {
+        id: 2,
+        name: "Best Selling",
+        link: "#/"
+    },
+    {
+        id: 3,
+        name: "Top Rated",
+        link: "#/"
+    },
+]
 
 const Navbar = () => {
   return (
@@ -43,6 +61,33 @@ const Navbar = () => {
                         </a>
                       </li>
                     ))}
+                    {/* Dropdown menu */}
+                    <li className="relative cursor-pointer group">
+                    <a href="#" className="flex items-center gap[2px]  font-semibold  text-gray-500 dark:hover:text-white py-2">
+                        Quicklinks
+                        <span>
+                        <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                     </span>
+                    </a>
+                   
+                    {/* Dropdown Links */}
+                    <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md">
+                        <ul className="space-y-2">
+                           {
+                            DropdownLinks.map((data, index) =>(
+                                <li>
+                                <a 
+                                className="text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                                href={data.link}>{data.name}
+                                </a>
+                                </li>
+                            ))
+                           } 
+                        </ul>
+
+
+                    </div>
+                    </li>
                   </ul>
                 </div>
               </div>
