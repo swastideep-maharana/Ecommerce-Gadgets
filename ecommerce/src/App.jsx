d73e9ab294becd869483401f2b63e25ba9302513
swastideep maharana <swastideep67maharana@gmail.com>
2024-06-19 11:19:33 +0530

    #navbar

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,16 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
-
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div>
+        <Navbar />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -4,8 +4,24 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#f42c37 ",
+        secondary: "f42c37",
+        brandYellow: "#fdc62e",
+        brandGreen: "#2dcc6f",
+        brandWhite: "#eeeeee",
+      },
+      container:{
+        center:true,
+        padding:{
+          DEFAULT: "1rem",
+          sm: "2rem",  
+        },
+      },
+    },
   },
   plugins: [],
 }
