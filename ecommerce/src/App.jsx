7e27c5cec8b7969b90bd965f454ed951b8fba3f1
swastideep maharana <swastideep67maharana@gmail.com>
2024-06-20 04:49:25 +0530

    #added-cards

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+        <Hero />
       </div>
     </>
   );
--- ecommerce/src/components/Navbar/Navbar.jsx
+++ ecommerce/src/components/Navbar/Navbar.jsx
@@ -79,7 +79,8 @@ const Navbar = () => {
                           {DropdownLinks.map((data, index) => (
                             <li>
                               <a
-                                className="text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                                className="text-gray-500 dark:hover:text-white duration-200
+                                inline-block w-full p-2 hover:bg-primary/20 rounded-md font-semibold"
                                 href={data.link}
                               >
                                 {data.name}
