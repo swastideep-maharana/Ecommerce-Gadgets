4941eb3a16eb54121aa33be565427cc70c02bbf6
swastideep maharana <swastideep67maharana@gmail.com>
2024-06-20 09:17:14 +0530

    #Added-service

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
+import Services from "./components/Services/services";
+
 
 function App() {
   return (
     <>
-      <div>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
         <Navbar />
         <Hero />
+        <Category />
+        <Category2 />
+        <Services/>
       </div>
     </>
   );
