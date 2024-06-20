8512a106211423878cc8c988cef17ed4579eba83
swastideep maharana <swastideep67maharana@gmail.com>
2024-06-21 02:08:01 +0530

    Added BannerData

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,36 @@
+
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
-import Services from "./components/Services/services";
+import Services from "./components/Services/services"; 
+import Banner from "./components/Banner/Banner";
+import headphone from "./assets/hero/headphone.png";
+import smartwatch from "./assets/category/smartwatch2-removebg-preview.png";
+import Products from "./components/Products/Products";
 
+const BannerData = {
+  discount: '30% OFF',
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Eaque reiciendis",
+  bgColor: "#f42c37",
+};
+
+const BannerData2 = {
+  discount: '30% OFF',
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: smartwatch, // Corrected variable name
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
 
 function App() {
   return (
@@ -15,6 +41,9 @@ function App() {
         <Category />
         <Category2 />
         <Services/>
+        <Banner data={BannerData} />
+        <Products />
+        <Banner data={BannerData2} />
       </div>
     </>
   );
